a4e1eca7b345f8adf9f57f46df44c325a74b0a04
Tomas Wallin <Myggski@users.noreply.github.com>
2021-08-31 09:16:48 +0200

    Delete build directory

--- build/server/repositories/Firebase.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var config_1 = require("../config");
-var app_1 = require("firebase/app");
-exports.default = (0, app_1.initializeApp)(config_1.firebaseConfig);
